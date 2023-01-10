cdcf9b6a2568ba3391f3269dc720aa38c6dcf32b
Stanislav Horochavcev <khorochavcev@mail.ru>
2023-01-10 23:09:32 +0400

    some change

---
+++ TWorld Project/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using TWorld_Project.Model;
+
+namespace TWorld_Project.Controllers
+{
+	public class UserController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Edit()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Save(UserInfo userModel)
+		{
+			return RedirectToAction("Index", userModel);
+        }
+    }
+}
+
---
+++ TWorld Project/Model/UserInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TWorld_Project.Model
+{
+    public class UserInfo
+    {
+        //Display don't work on view
+
+        public UserInfo()
+        {
+            User_Name = "Stas";
+            About_User = "Stats120120";
+        }
+
+        [Display(Name = "Ваше Имя")]
+        [Required(ErrorMessage = "Необходимо ввести имя")]
+        public string User_Name { get; set; }
+
+        [Required(ErrorMessage = "Необходимо ввести почту")]
+        [Display(Name = "Почта")]
+        public string User_Email { get; set; }
+
+        [Required(ErrorMessage = "Необходимо ввести номер телефона")]
+        public string Phone_Number { get; set; }
+
+        [Required(ErrorMessage = "Необходимо ввести город")]
+        public string User_City { get; set; }
+
+        [Required(ErrorMessage = "Необходимо ввести информацию о себе")]
+        public string About_User { get; set; }
+    }
+}
